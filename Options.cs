45fff7a73ec09e18b11770cee3d89f9a51186bfa
Tyler Clement <9252928+Rhexis@users.noreply.github.com>
2024-09-26 16:57:14 +0800

    feat: add text hiding in png images + basic framework

---
+++ Options.cs
@@ -0,0 +1,36 @@
+using CommandLine;
+
+namespace Stegosaurus;
+
+public enum Mode
+{
+    hide,
+    fetch
+}
+
+public enum Type
+{
+    image,
+    text
+}
+
+public class Options
+{
+    [Option('m', "mode", Required = true, HelpText = "The mode of operation, 'hide' or 'fetch'.")]
+    public Mode Mode { get; set; }
+    
+    [Option('t', "type", Required = false, HelpText = "Type of content to hide, 'text' or 'image'.")]
+    public Type Type { get; set; }
+    
+    // For mode "fetch"
+    [Option('s', "src", Required = false, HelpText = "Source of the image containing the hidden data.")]
+    public string Source { get; set; }
+    
+    // For mode "hide"
+    [Option('c', "content", Required = false, HelpText = "Location of image or text to hide.")]
+    public string Content { get; set; }
+    [Option('i', "input", Required = false, HelpText = "The image to hide content in.")]
+    public string Input { get; set; }
+    [Option('o', "output", Required = false, HelpText = "The location & name of the desired output file.")]
+    public string Output { get; set; }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Stegosaurus
+# Steg-o-saurus
+
+To run: `./bin/debug/net8.0/Stegosaurus`
+
+To Hide: `./bin/debug/net8.0/Stegosaurus -m hide -t text -c "Hello World" -i "./steg.png" -o "./output.png"`
+To Hide: `./bin/debug/net8.0/Stegosaurus -m hide -t text -c "This is a test :)" -i "./steg.png" -o "./output.png"`
+
+To Fetch: `./bin/debug/net8.0/Stegosaurus -m fetch -t text -s "./output.png"`
+
+To Show diff:
+1. `brew install imagemagick`
+2. `magick compare ./steg.png ./output.png ./diff.png`
